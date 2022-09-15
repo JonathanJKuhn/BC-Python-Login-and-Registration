77a2637560594e1fa3aa5fa29a24567989b1e601
Jonathan Kuhn <jkuhn96@gmail.com>
2022-09-15 15:35:52 -0400

    Added login ft. and more validation

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,5 +1,5 @@
 from flask_app import app
-from flask import render_template, redirect, request, session
+from flask import render_template, redirect, request, session, flash
 from flask_bcrypt import Bcrypt
 from flask_app.models.user import User
 
@@ -25,20 +25,30 @@ def register():
             session[key] = data[key]
         return redirect('/')
     
-    session.clear()
     pw_hash = bcrypt.generate_password_hash(request.form.get('password'))
     data['password'] = pw_hash
-    new_user = {
-        'id': User.add(data)
-    }
-    user_obj = User.get_user(new_user)
-    for attr, value in user_obj.__dict__.items():
-        session[attr] = value
+    new_user = User.add(data)
+    session['id'] = new_user
+    return redirect('/dashboard')
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = { 'email': request.form.get('email') }
+    user_in_db = User.get_user_by_email(data)
+    if not user_in_db:
+        flash("Invalid Email/Password")
+        return redirect('/')
+    if not bcrypt.check_password_hash(user_in_db.password, request.form.get('password')):
+        flash("Invalid Email/Password")
+        return redirect('/')
+    session['id'] = user_in_db.id
     return redirect('/dashboard')
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html',title='Dashboard')
+    data = { 'id': session['id'] }
+    user_data = User.get_user_by_id(data)
+    return render_template('dashboard.html',title='Dashboard',user=user_data)
 
 @app.route('/logout')
 def logout():
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -3,6 +3,8 @@ from flask import flash
 import re
 
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+NAME_REGEX = re.compile(r'^[a-zA-Z]{2,}$')
+PW_REGEX = re.compile(r'(?=.*\d)(?=.*[A-Z])')
 
 class User:
     def __init__(self, db_data) -> None:
@@ -22,32 +24,43 @@ class User:
         return result
 
     @classmethod
-    def get_user(cls, data):
+    def get_user_by_id(cls, data):
         query = "SELECT * FROM users WHERE id = %(id)s;"
         result = connectToMySQL('login_schema').query_db(query, data)
-        print(result[0])
+        return cls(result[0])
+        
+    @classmethod
+    def get_user_by_email(cls, data):
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        result = connectToMySQL('login_schema').query_db(query, data)
         return cls(result[0])
 
     @staticmethod
     def validate_registration(user):
         is_valid = True
-        if len(user['fname']) <= 0:
-            flash("First Name must not be empty.")
+        if len(user['fname']) < 2:
+            flash("First Name isn't long enough")
             is_valid = False
-        if len(user['lname']) <= 0:
-            flash("Last Name must not be empty.")
+        if len(user['lname']) < 2:
+            flash("Last Name isn't long enough")
+            is_valid = False
+        elif not NAME_REGEX.match(user['fname']) or not NAME_REGEX.match(user['lname']):
+            flash("Name must only contain letters")
             is_valid = False
         if not EMAIL_REGEX.match(user['email']):
-            flash("Invalid email address.")
+            flash("Invalid email address")
             is_valid = False
-        if len(user['password']) <= 0:
-            flash("Password must not be empty.")
+        if len(user['password']) < 8:
+            flash("Password isn't long enough")
+            is_valid = False
+        elif not PW_REGEX.match(user['password']):
+            flash("Please inlcude at least 1 number and 1 uppercase letter")
             is_valid = False
         if len(user['confirm']) <= 0:
-            flash("Please confirm password.")
+            flash("Please confirm password")
             is_valid = False
         if not user['password'] == user['confirm']:
-            flash("Password does not match Confirm Password.")
+            flash("Password does not match Confirm Password")
             is_valid = False
 
         if is_valid:
